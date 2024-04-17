44bbf9c56e1eb488b26df5d914176e0beaec430c
Daniel Deusing <daniel.deusing@twiced.de>
2024-04-17 21:37:15 +0200

    refactored create_manifest.py

--- create_manifest.py
+++ create_manifest.py
@@ -1,23 +1,25 @@
 import os
 import json
 
+
 def format_name(directory_name):
-    """Format directory name by inserting spaces before capital letters in concatenated words."""
-    # Split at the first dash if the prefix is a number
+    """Inserts spaces before capital letters to format concatenated words, unless they are acronyms."""
     name_parts = directory_name.split('-', 1)
     if len(name_parts) > 1:
         directory_name = name_parts[1]
 
-    # Insert space before capital letters
-    formatted_name = ""
-    for i, char in enumerate(directory_name):
-        if char.isupper() and i != 0 and not directory_name[i - 1].isspace():
+    formatted_name = directory_name[0]
+    for i in range(1, len(directory_name)):
+        if (directory_name[i].isupper() and
+            not directory_name[i-1].isspace() and
+            not directory_name[i-1].isupper()):
             formatted_name += ' '
-        formatted_name += char
+        formatted_name += directory_name[i]
     return formatted_name
 
+
 def get_directories_and_files(_path):
-    """Return a manifest of directories and files."""
+    """Builds a manifest of directories and files with formatted names."""
     _manifest = {"folders": []}
     for item in os.listdir(_path):
         full_path = os.path.join(_path, item)
@@ -31,18 +33,12 @@ def get_directories_and_files(_path):
             })
     return _manifest
 
-# Directory to scan
+
 path = '.'
 
-# Get the manifest
 manifest = get_directories_and_files(path)
 
-# JSON output
 json_output = json.dumps(manifest, indent=4)
 
-# Print to console
-print(json_output)
-
-# Write to file
 with open('manifest.json', 'w') as file:
     file.write(json_output)
